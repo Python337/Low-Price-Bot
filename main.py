d8dbc6b3b597980feed870ac0d5c5e537674f99b
The_one <141361325+Python337@users.noreply.github.com>
2024-02-24 21:35:04 +0300

    Убрал ненужный ранее написанный код, так как изменились немного задачи этого бота

--- main.py
+++ main.py
@@ -39,44 +39,6 @@ def save_username(message):
     keyboard.add(button_price, button_price2, button_price3)
 
 
-def save_surname(message):
-    chat_id = message.chat.id
-    surname = message.text
-    users[chat_id]['surname'] = surname
-    keyboard = telebot.types.InlineKeyboardMarkup()
-    button_save = telebot.types.InlineKeyboardButton(text="Сохранить")
-    button_change = telebot.types.InlineKeyboardButton(text="Изменить")
-
-    keyboard.add(button_save, button_change)
-
-    bot.send_message(chat_id, f'Сохранить данные?', reply_markup=keyboard)
-
-
-@bot.message_handler(commands=['who_i'])
-def who_i(message):
-    chat_id = message.chat.id
-    name = users[chat_id]['name']
-    surname = users[chat_id]['surname']
-    bot.send_message(chat_id, f'Вы: {name} {surname}')
-
-
-@bot.callback_query_handler(func=lambda call: call.data == 'save_data')
-def save_btn(call):
-    message = call.message
-    chat_id = message.chat.id
-    message_id = message.message_id
-    bot.edit_message_text(chat_id=chat_id, message_id=message_id,
-                         text='Данные сохранены!')
-
-
-@bot.callback_query_handler(func=lambda call: call.data == 'change_data')
-def save_btn(call):
-    message = call.message
-    chat_id = message.chat.id
-    message_id = message.message_id
-    bot.edit_message_text(chat_id=chat_id, message_id=message_id,
-                         text='Изменение данных!')
-    write_to_support(message)
 
 
 if __name__ == '__main__':
