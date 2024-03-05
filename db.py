1cc4f4e19cc8fcf4e7c1be2548385740bd24d5fd
Python337 <ivanzarapin26@mail.ru>
2024-03-05 22:33:04 +0300

    Привязал базу данных к проекту.

---
+++ db.py
@@ -0,0 +1,8 @@
+import pymysql
+
+
+connection = pymysql.connect(host='147.45.105.54',
+                             user='gen_user',
+                             password='mfohmdu%E3$b\k',
+                             database='default_db',
+)
--- main.py
+++ main.py
@@ -16,11 +16,17 @@ def welcome(message):
     bot.send_message(chat_id,
                      'Добро пожаловать в бота для поиска товаров на складе. Этот бот напишет, где хранится интересующий товар.',
                      reply_markup=keyboard)
+
+
 @bot.message_handler(func=lambda message: message.text == 'Выход на главное меню')
 def back_to_menu(message):
     welcome(message)
+
+
 button_save = telebot.types.InlineKeyboardButton(text="Сохранить",
-                                                     callback_data='save_data')
+                                                 callback_data='save_data')
+
+
 @bot.message_handler(
     func=lambda message: message.text == 'Поиск товара')
 def write_to_support(message):
@@ -33,20 +39,18 @@ def buttons_price(message):
     chat_id = message.chat.id
     keyboard = telebot.types.InlineKeyboardMarkup()
     button_price = telebot.types.InlineKeyboardButton(text="Дешёво",
-        callback_data = 'cheaply')
+                                                      callback_data='cheaply')
 
     button_price2 = telebot.types.InlineKeyboardButton(text="Средняя",
                                                        callback_data='average')
 
     button_price3 = telebot.types.InlineKeyboardButton(text="Дорого",
-    callback_data='expensive')
+                                                       callback_data='expensive')
 
     keyboard.add(button_price, button_price2, button_price3)
     bot.send_message(chat_id,
                      f'Отлично. Теперь укажите по ценовому диапозону товар, так как возможно есть несколько разных производителей данного товара:',
                      reply_markup=keyboard)
-
-
 
 
 if __name__ == '__main__':
