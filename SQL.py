7900aa0d1ade24739796a77e349149e3442066c6
The_one <141361325+Python337@users.noreply.github.com>
2024-04-13 23:14:11 +0300

    В базе данных добавил больше строительных товаров и характеристику к  ним.

---
+++ SQL.py
@@ -0,0 +1,7 @@
+import pymysql
+
+connection = pymysql.connect(host='147.45.105.54',
+                             user='gen_user',
+                             password='|2?xzWwGa+KF\\B',
+                             database='favorable_prices',
+)
---
+++ main2.py
@@ -0,0 +1,64 @@
+import telebot
+from config import TOKEN
+
+bot = telebot.TeleBot(TOKEN)
+
+users = {}
+
+
+@bot.message_handler(commands=['start'])
+def welcome(message):
+    chat_id = message.chat.id
+    keyboard = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
+    button_price = telebot.types.KeyboardButton(text='Выход на главное меню')
+    button_price2 = telebot.types.KeyboardButton(text='Поиск товара')
+    keyboard.add(button_price, button_price2)
+    bot.send_message(chat_id,
+                     'Добро пожаловать в бота для поиска товаров на складе. Этот бот напишет, где хранится интересующий товар.',
+                     reply_markup=keyboard)
+@bot.message_handler(func=lambda message: message.text == 'Поиск товара')
+# def bd_buttons(message):
+#     chat_id = message.chat.id
+#     keyboard = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
+#     button_price = telebot.types.KeyboardButton(text='Выход на главное меню')
+#     button_price2 = telebot.types.KeyboardButton(text='Поиск товара')
+#     keyboard.add(button_price, button_price2)
+
+
+
+@bot.message_handler(func=lambda message: message.text == 'Выход на главное меню')
+def back_to_menu(message):
+    welcome(message)
+
+
+button_save = telebot.types.InlineKeyboardButton(text="Сохранить",
+                                                 callback_data='save_data')
+
+
+@bot.message_handler(
+    func=lambda message: message.text == 'Поиск товара')
+def write_to_support(message):
+    chat_id = message.chat.id
+    bot.send_message(chat_id, 'Введите название товара: ')
+    bot.register_next_step_handler(message, buttons_price)
+
+
+def buttons_price(message):
+    chat_id = message.chat.id
+    keyboard = telebot.types.InlineKeyboardMarkup()
+    button_price = telebot.types.InlineKeyboardButton(text="Дешёво",
+                                                      callback_data='cheaply')
+
+    button_price2 = telebot.types.InlineKeyboardButton(text="Средняя",
+                                                       callback_data='average')
+
+    button_price3 = telebot.types.InlineKeyboardButton(text="Дорого",
+                                                       callback_data='expensive')
+
+    keyboard.add(button_price, button_price2, button_price3)
+    bot.send_message(chat_id,
+                     f'Отлично. Теперь укажите по ценовому диапозону товар, так как возможно есть несколько разных производителей данного товара:',
+                     reply_markup=keyboard)
+if __name__ == '__main__':
+    print('Бот запущен!')
+    bot.infinity_polling()
